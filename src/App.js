4f04d1bba63f2245f27eb7698f233e75c4919e56
osbornnick <nicholas.p.osborn@gmail.com>
2021-11-29 16:17:52 -0500

    add react-router-dom and App.js routes to login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import Login from "./components/login";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <div className="container">
+                <Route path="/login" component={Login} />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
