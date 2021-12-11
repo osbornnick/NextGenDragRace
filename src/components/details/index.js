2164963c690c0d128af0d68a116699e3409da617
Nick Osborn <nicholas.p.osborn@gmail.com>
2021-12-11 15:06:43 -0500

    Details main page should not be queen specific

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 const Details = () => {
     return (
         <div>
-            <h1>Queen Zone Alert</h1>
+            <h1>Details</h1>
             <Outlet />
         </div>
     );
