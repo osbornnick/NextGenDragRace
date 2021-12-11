f451bbc1bd5ab6023bfdf332443c5e135dee2928
Nick Osborn <nicholas.p.osborn@gmail.com>
2021-12-11 15:40:06 -0500

    messing around with queen details styling

--- src/components/details/queens/index.js
+++ src/components/details/queens/index.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { getQueenById } from "../../../services/queenService";
 import { useParams } from "react-router-dom";
 import CommentSection from "../../comment";
-import "./queenImage.css"
+import "./queenImage.css";
 
 const selectQueen = (state) => state.queen;
 
@@ -14,80 +14,91 @@ const Details = (props) => {
     useEffect(() => {
         getQueenById(dispatch, params.id);
     }, []);
+    if (!queen) return "Loading";
     return (
         <>
-            <div className = "row mb-3">
-                <div className = "col-6">
-                    <img src={queen.image_url} className="wd-queen-image mb-4"/>
-                    <h4>
-                        Season appearances & ranking:
-                    </h4>
-                        {
-                            queen.seasons.map(season => {
-                                return (
-                                    <li className="list-group-item p-1 mb-2">
-                                        <i className="fas fa-star"></i>
-                                        <span > Season {season.seasonNumber} </span>
-                                            <span> Rank {season.place}</span>
-                                    </li>
-                                )
-                            })
-                        }
-                        <br/>
-                        <h4>
-                            Episodes:
-                        </h4>
-                        {
-                            queen.episodes.map(episode => {
-                                return (
-                                    <li className="list-group-item p-2">
-                                        <i className="fas fa-video me-1"></i>
-                                        <span>"{episode.title}"</span>
-                                        <span > season {episode.seasonId} </span>
-                                        <span className="me-1"> episode {episode.episodeInSeason}</span>
-                                        <span>({episode. airDate})</span>
-
-
-                                    </li>
-                                )
-                            })
-                        }
-
+            <div className="row mb-3">
+                <div className="col-6">
+                    <img
+                        src={queen.image_url}
+                        className="mb-4 img-fluid align-self-end"
+                        style={{ maxWidth: "30rem", borderRadius: "10%" }}
+                        alt="queens profile"
+                    />
                 </div>
                 <div className="col-6">
-                    <h2>
-                        Introducing queen {queen.name}
-                    </h2>
-                    <br/>
-                    <p >
+                    <h1>{queen.name}</h1>
+                    <div>
                         <h4 className="mt-2">Epic quote: </h4>
-                        <i className="fas fa-hand-holding-heart me-1"></i>
-                        <span>{queen.quote}</span>
-                    </p>
-                    <br/>
-                    <h4>
-                        Challenges the queen had been through:
-                    </h4>
+                        <div className="list-group">
+                            <div className="list-group-item">
+                                <i className="fas fa-hand-holding-heart me-1"></i>
+                                <span>{queen.quote}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-6">
+                    <h4>Season appearances & ranking:</h4>
+                    <ul className="list-group">
+                        {queen.seasons.map((season) => {
+                            return (
+                                <li className="list-group-item">
+                                    <i className="fas fa-star me-2"></i>
+                                    <span> Season {season.seasonNumber} </span>
+                                    <span> Rank {season.place}</span>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
+                <div className="col-6">
+                    <h4>Episodes:</h4>
+                    <ul className="list-group">
+                        {queen.episodes.map((episode) => {
+                            return (
+                                <li className="list-group-item">
+                                    <i className="fas fa-video me-2"></i>
+                                    <span>"{episode.title}"</span>
+                                    <span> season {episode.seasonId} </span>
+                                    <span className="me-1">
+                                        {" "}
+                                        episode {episode.episodeInSeason}
+                                    </span>
+                                    <span>({episode.airDate})</span>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </div>
 
-                        {
-                            queen.challenges.map(challenge => {
-                                return (
-                                    <li className="list-group-item">
-                                        {challenge.won && <i className="fas fa-trophy me-1"></i>}
-                                        {challenge.description}
-                                    </li>
-                                )
-                            })
-                        }
-
+                <div className="col-6">
+                    <h4>Challenges:</h4>
+                    <ul className="list-group">
+                        {queen.challenges.map((challenge) => {
+                            return (
+                                <li className="list-group-item d-flex align-items-center">
+                                    {challenge.description}
+                                    {challenge.won && (
+                                        <i
+                                            className="fas fa-trophy ms-1"
+                                            style={{ color: "#FFD700" }}
+                                        ></i>
+                                    )}
+                                </li>
+                            );
+                        })}
+                    </ul>
                 </div>
             </div>
-
-            {queen ? (
-                <CommentSection parentEntityType="queens" parentId={queen.id} />
-            ) : (
-                ""
-            )}
+            <div className="row">
+                <div className="col">
+                    <CommentSection
+                        parentEntityType="queens"
+                        parentId={queen.id}
+                    />
+                </div>
+            </div>
         </>
     );
 };
