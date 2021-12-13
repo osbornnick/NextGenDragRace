c9386b1f1ca5636d87dd0ff242127d680b06abc4
Nick Osborn <nicholas.p.osborn@gmail.com>
2021-12-13 11:39:12 -0500

    responsive footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,14 +3,30 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <footer className="footer mt-auto py-3">
-            <div className="container d-flex justify-content-center">
-                <Link to="/privacy" className="px-3 text-muted">
-                    Privacy Policy
-                </Link>
-                <Link to="/login" className="px-3 text-muted">
-                    Login
-                </Link>
-                <span className="px-3">© 2021 Drag Homies Inc.</span>
+            <div className="container">
+                <div className="row d-flex justify-content-center">
+                    <div className="col-md-12 col-xl-2">
+                        <Link to="/privacy" className="px-3 text-muted">
+                            Privacy Policy
+                        </Link>
+                    </div>
+                    <div className="col-md-12 col-xl-1">
+                        <Link to="/login" className="px-3 text-muted">
+                            Login
+                        </Link>
+                    </div>
+                    <div className="col-md-12 col-xl-3">
+                        <span className="px-3">© 2021 Drag Homies Inc.</span>
+                    </div>
+                    <div className="col-md-12 col-xl-3">
+                        <span className="px-3">
+                            Powered by{" "}
+                            <a href="https://drag-race-api.readme.io/docs">
+                                NoKeyNoShade
+                            </a>
+                        </span>
+                    </div>
+                </div>
             </div>
         </footer>
     );
