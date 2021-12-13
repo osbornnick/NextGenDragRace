a4c8c257b45b0fb861e2a02d7462a87aef2d269f
Zobby <zipprobertt@gmail.com>
2021-12-12 18:53:32 -0800

    add text / context to home

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,6 +3,7 @@ import { paginateQueens } from "../../services/queenService";
 import { useDispatch, useSelector } from "react-redux";
 import { QueenImage } from "./queenCard";
 
+
 const selectQueens = (state) => state.queens;
 
 const Home = () => {
@@ -13,12 +14,26 @@ const Home = () => {
     }, []);
     return (
         <>
-            <h1>Home</h1>
+            <h1>NextGenDragRace: Home</h1>
+            <h4 className= "text-light">
+                All of the tea on the queens of Drag Race, in a non-boring layout.
+            </h4>
+            <p> Want to create your own dream team of queens? Register up above and join the kiki!</p>
+            <h3> _ </h3>
+            <div className="container">
+            <div className="row d-flex flex-row ">
+                <div className="col">
+                <h3> Season (season_num) </h3>
+                </div>
+            </div>
             <div className="row d-flex flex-row">
+                <h5>Click on a queen to view their stats and track record on the show </h5>
                 {queens
                     ? queens.map((q, i) => <QueenImage queen={q} key={q.id} />)
                     : "loading queens"}
+                </div>
             </div>
+            <div> Powered by NoKeyNoShade API </div>
         </>
     );
 };
